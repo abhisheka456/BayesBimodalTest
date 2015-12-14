732d582155572f38af8ecf0c8192fa0e0a9adb42
Gregory Ashton <gash789@gmail.com>
2015-12-14 10:13:00 +0000

    Add simple setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='BayesBimodalTest',
+      packages=[''],
+      version='0.1',
+      description='',
+      requires=[],
+      author='Greg Ashton',
+      author_email='gash789@gmail.com')
