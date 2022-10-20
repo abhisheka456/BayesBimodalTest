fe0ab78f62a298f822163bb528a9c21cd8bce5b2
Abhishek Kumar <47485978+abhisheka456@users.noreply.github.com>
2022-10-20 11:40:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(name='BayesBimodalTest',
       packages=[''],
-      version='0.1',
+      version='0.2',
       description='',
       requires=[],
       author='Greg Ashton',
